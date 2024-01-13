3bf4c37047af117aca57b47c09fea561f7ba32ee
ZX <zx@zxhack.com>
2024-01-14 00:13:16 +0900

    細かな修正

--- NamedPipeSample/Program.cs
+++ NamedPipeSample/Program.cs
@@ -1,18 +1,12 @@
 ﻿using System.Diagnostics;
 
 var pipeServer = new Process();
-var pipeServer2 = new Process();
 var pipeClient = new Process();
 
 pipeServer.StartInfo.FileName = "NamedPipeServer.exe";
 pipeServer.StartInfo.Arguments = "pipe1";
 pipeServer.StartInfo.UseShellExecute = true;
 pipeServer.Start();
-
-pipeServer2.StartInfo.FileName = "NamedPipeServer.exe";
-pipeServer2.StartInfo.Arguments = "pipe1";
-pipeServer2.StartInfo.UseShellExecute = true;
-pipeServer2.Start();
 
 pipeClient.StartInfo.FileName = "NamedPipeClient.exe";
 pipeClient.StartInfo.Arguments = "pipe1";
